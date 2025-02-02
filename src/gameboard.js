6fad6b957bd0d90c8930eff1a2dbeac9d796e6b5
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2025-02-02 09:48:29 -0800

    feat: Gameboard, Gameboard.board

---
+++ src/gameboard.js
@@ -0,0 +1,17 @@
+import { Ship } from './ships';
+
+// No.	Class of ship	  Size
+// 1	    Carrier	        5
+// 2	    Battleship	    4
+// 3	    Cruiser	        3
+// 4	    Submarine	      3
+// 5	    Destroyer	      2
+
+export class Gameboard {
+  constructor() {
+    // 10 x 10
+    this.board = Array(10).fill(Array(10).fill(null));
+  }
+
+  placeShip() {}
+}
---
+++ src/tests/gameboard.test.js
@@ -0,0 +1,7 @@
+import { Ship } from '../ships';
+import { Gameboard } from '../gameboard';
+
+test('place ship', () => {
+  const gameboard = new Gameboard();
+  console.log(gameboard.board);
+});
