50518c9b76a809a4f910d99e08a870edd6de9c41
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2025-02-05 20:00:21 -0800

    html/style: add game board cells into rows

--- src/game-controller.js
+++ src/game-controller.js
@@ -2,7 +2,7 @@ import { Player } from './player';
 
 export class GameController {
   constructor() {
-    this.player = new Player();
+    this.human = new Player();
     this.computer = new Player();
   }
 }
--- src/game-display.js
+++ src/game-display.js
@@ -2,18 +2,38 @@ import { GameController } from './game-controller';
 
 export class GameDisplay {
   constructor() {
-    this.playerBoard = document.querySelector('.game__board--player');
-    this.computerBoard = document.querySelector('game__board--computer');
+    this.humanBoard = document.querySelector('.game__board--human');
+    this.computerBoard = document.querySelector('.game__board--computer');
     this.game = new GameController();
+    this.human = this.game.human;
+    this.computer = this.game.computer;
+    this.displayGameboard(this.humanBoard);
+    this.displayGameboard(this.computerBoard);
   }
 
   displayGameboard(board) {
-    let count = 100;
-    while (count > 0) {
-      const cell = document.createElement('div');
-      cell.classList.add('game__board--cell');
-      board.appendChild(cell);
-      count--;
-    }
+    const rows = [...board.children];
+    rows.forEach((row) => {
+      for (let i = 0; i < 10; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('game__board__cell');
+        row.appendChild(cell);
+      }
+    });
+  }
+
+  getCell(coord) {
+    const [col, row] = [coord[0], 9 - coord[1]];
+  }
+
+  placeShip(board, shipName, coord, direction) {}
+
+  placeShipsRandomly(board) {
+    const ships = Object.keys(board.gameBoard.ships);
+    board.placeShip('carrier', [1, 2], 'horizontal');
+  }
+
+  cellRecieveAttack(cell) {
+    cell.addEventListener('click', () => {});
   }
 }
