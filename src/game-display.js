08d238e3fb7535ee946b3a44210d577dfdd10bd2
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2025-02-07 20:53:42 -0800

    feat: GameDisplay changeCellDataState
    Changes cell data-state to 'empty' if player.gameboard is null.
    Otherwise updates cell data-state to player.gameboard value.

--- src/game-display.js
+++ src/game-display.js
@@ -7,16 +7,17 @@ export class GameDisplay {
     this.game = new GameController();
     this.human = this.game.human;
     this.computer = this.game.computer;
-    this.displayGameboard(this.humanBoard);
-    this.displayGameboard(this.computerBoard);
+    this.initGameboard(this.humanBoard);
+    this.initGameboard(this.computerBoard);
   }
 
-  displayGameboard(board) {
+  initGameboard(board) {
     const rows = [...board.children];
     rows.forEach((row) => {
       for (let i = 0; i < 10; i++) {
         const cell = document.createElement('div');
         cell.classList.add('game__board__cell');
+        cell.setAttribute('data-state', 'empty');
         row.appendChild(cell);
       }
     });
@@ -27,6 +28,23 @@ export class GameDisplay {
     const boardRow = [...board.children][row];
     const boardCell = [...boardRow.children][col];
     return boardCell;
+  }
+
+  changeCellDataState(player, coord) {
+    let displayBoard = null;
+    if (displayBoard === this.human) displayBoard = this.humanBoard;
+    if (displayBoard === this.computer) displayBoard = this.computerBoard;
+
+    const cell = this.getBoardCell(displayBoard, coord);
+    if (!player.gameBoard(coord)) {
+      cell.setAttribute('data-state', 'empty');
+      return;
+    } else cell.setAttribute(player.gameBoard(coord));
+  }
+
+  displayShipsRandomly(player) {
+    player.gameBoard.displayShipsRandomly();
+    player.gameBoard.forEach((row) => {});
   }
 
   placeShip(board, shipName, coord, direction) {}
