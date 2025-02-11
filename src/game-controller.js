865e1fcb5440ba4f75f45d94032c6bc5b75ceaad
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2025-02-10 21:06:37 -0800

    feat: GameController computerAttack()

--- src/game-controller.js
+++ src/game-controller.js
@@ -9,6 +9,17 @@ export class GameController {
     this.display = new GameDisplay();
     this.humanDisplay = this.display.humanBoard;
     this.computerDisplay = this.display.computerBoard;
+
+    this.openCells = this.possibleCoords();
+  }
+
+  possibleCoords(arr = []) {
+    for (let row = 0; row <= 9; row++) {
+      for (let col = 0; col <= 9; col++) {
+        arr.push([row, col]);
+      }
+    }
+    return arr;
   }
 
   getDisplayBoard(player) {
@@ -45,5 +56,19 @@ export class GameController {
       const board = this.getDisplayBoard(player);
       this.display.getDisplayBoardCell(board, coord).textContent = 'X';
     }
+    return result;
+  }
+
+  computerAttack() {
+    if (this.openCells.length === 0) return null; // game should be over
+    const randIdx = Math.floor(Math.random() * this.openCells.length);
+    const lastEle = this.openCells.length - 1;
+
+    const temp = this.openCells[randIdx];
+    this.openCells[randIdx] = this.openCells[lastEle];
+    this.openCells[lastEle] = temp;
+
+    const coord = this.openCells.pop();
+    this.attackPlayer(this.human, coord);
   }
 }
